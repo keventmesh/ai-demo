57bb6d9f85731ea5a2580293d58454ea76ccada6
Ali Ok <aliok@redhat.com>
2023-10-06 10:56:09 +0300

    Use Python context managers for spans

--- tests/performance-test/lib.py
+++ tests/performance-test/lib.py
@@ -1,5 +1,6 @@
 import asyncio
 import base64
+import uuid
 
 import aiohttp
 import socketio
@@ -10,8 +11,12 @@ DEFAULT_HTTP_REQUEST_TIMEOUT = 3600
 logger = logging.getLogger(__name__)
 
 
-async def upload(image_path, upload_service_url):
+async def upload(image_path, upload_service_url, fake):
     logger.debug(f"upload '{image_path}' to {upload_service_url}")
+
+    if fake:
+        await asyncio.sleep(0.1)
+        return True, "fake_" + uuid.uuid4().hex
 
     with open(image_path, mode='rb') as file:
         image_binary = file.read()
@@ -37,15 +42,20 @@ async def upload(image_path, upload_service_url):
                 if 'uploadId' not in response_json:
                     raise Exception(
                         f"Missing uploadId in response body from {upload_service_url} for image {image_path}")
-                logger.debug(f"Uploaded image {image_path} to {upload_service_url} with uploadId {response_json['uploadId']}")
+                logger.debug(
+                    f"Uploaded image {image_path} to {upload_service_url} with uploadId {response_json['uploadId']}")
                 return True, response_json['uploadId']
     except Exception as e:
         logger.debug(f"Failed to upload image {image_path} to {upload_service_url}: {e}")
         return False, str(e)
 
 
-async def init_reply_ws_connection(reply_service_url):
+async def init_reply_ws_connection(reply_service_url, fake):
     logger.debug(f"init reply ws connection to {reply_service_url}")
+
+    if fake:
+        await asyncio.sleep(0.1)
+        return True, {}
 
     # connector = aiohttp.TCPConnector(ssl=False)
     # http_session = aiohttp.ClientSession(connector=connector)
@@ -60,7 +70,11 @@ async def init_reply_ws_connection(reply_service_url):
         return False, str(e)
 
 
-async def wait_for_reply(conn, upload_id):
+async def wait_for_reply(conn, upload_id, fake):
+    if fake:
+        await asyncio.sleep(0.1)
+        return True, {"uploadId": "fake_" + uuid.uuid4().hex, "probability": 0.5, "x0": 0.1, "x1": 0.2, "y0": 0.3, "y1": 0.4, "created_on": "2021-01-01T00:00:00.000Z"}
+
     loop = asyncio.get_running_loop()
     fut = loop.create_future()
 
@@ -101,8 +115,12 @@ async def wait_for_reply(conn, upload_id):
         return False, str(e)
 
 
-async def send_feedback(upload_id, feedback, feedback_service_url):
+async def send_feedback(upload_id, feedback, feedback_service_url, fake):
     logger.debug(f"Send feedback for {upload_id} to {feedback_service_url}")
+
+    if fake:
+        await asyncio.sleep(0.1)
+        return True, None
 
     data = {
         "uploadId": upload_id,
@@ -118,4 +136,18 @@ async def send_feedback(upload_id, feedback, feedback_service_url):
         return True, None
     except Exception as e:
         logger.debug(f"Failed to send feedback for {upload_id} to {feedback_service_url}: {e}")
+        return False, str(e)
+
+async def disconnect_ws(conn, fake):
+    logger.debug(f"Disconnect from reply service")
+
+    if fake:
+        await asyncio.sleep(0.1)
+        return True, None
+
+    try:
+        await conn.disconnect()
+        return True, None
+    except Exception as e:
+        logger.debug(f"Failed to disconnect from reply service: {e}")
         return False, str(e)
