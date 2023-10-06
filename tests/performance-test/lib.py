aaa556cbad30913763e240c6df2bcfca382e1726
Ali Ok <aliok@redhat.com>
2023-10-06 13:31:02 +0300

    Shite

--- tests/performance-test/lib.py
+++ tests/performance-test/lib.py
@@ -7,6 +7,7 @@ import socketio
 import logging
 
 DEFAULT_HTTP_REQUEST_TIMEOUT = 3600
+DEFAULT_WS_CONNECTION_TIMEOUT=3600
 
 logger = logging.getLogger(__name__)
 
@@ -46,7 +47,8 @@ async def upload(image_path, upload_service_url, fake):
                     f"Uploaded image {image_path} to {upload_service_url} with uploadId {response_json['uploadId']}")
                 return True, response_json['uploadId']
     except Exception as e:
-        logger.debug(f"Failed to upload image {image_path} to {upload_service_url}: {e}")
+        logger.warning(f"Failed to upload image {image_path} to {upload_service_url}: {e}")
+        logger.warning(e, exc_info=True)
         return False, str(e)
 
 
@@ -62,18 +64,20 @@ async def init_reply_ws_connection(reply_service_url, fake):
     # http_session.verify = False
     # sio = socketio.AsyncClient(http_session=http_session)
     try:
-        sio = socketio.AsyncClient()
+        sio = socketio.AsyncClient(request_timeout=DEFAULT_WS_CONNECTION_TIMEOUT, logger=logger.isEnabledFor(logging.DEBUG), engineio_logger=logger.isEnabledFor(logging.DEBUG))
         await sio.connect(reply_service_url, transports=['websocket'])
         return True, sio
     except Exception as e:
-        logger.debug(f"Failed to connect to {reply_service_url}: {e}")
+        logger.warning(f"Failed to connect to {reply_service_url}: {e}")
+        logger.warning(e, exc_info=True)
         return False, str(e)
 
 
 async def wait_for_reply(conn, upload_id, fake):
     if fake:
         await asyncio.sleep(0.1)
-        return True, {"uploadId": "fake_" + uuid.uuid4().hex, "probability": 0.5, "x0": 0.1, "x1": 0.2, "y0": 0.3, "y1": 0.4, "created_on": "2021-01-01T00:00:00.000Z"}
+        return True, {"uploadId": "fake_" + uuid.uuid4().hex, "probability": 0.5, "x0": 0.1, "x1": 0.2, "y0": 0.3,
+                      "y1": 0.4, "created_on": "2021-01-01T00:00:00.000Z"}
 
     loop = asyncio.get_running_loop()
     fut = loop.create_future()
@@ -89,6 +93,7 @@ async def wait_for_reply(conn, upload_id, fake):
 
     def on_connect_error(data):
         logger.debug(f"Connection error for upload_id {upload_id}: {data}")
+        fut.set_exception(Exception(f"Connection error for upload_id {upload_id}: {data}"))
 
     def on_disconnect():
         logger.debug(f"Disconnected from reply service. upload_id: {upload_id}")
@@ -102,7 +107,8 @@ async def wait_for_reply(conn, upload_id, fake):
         logger.debug(f"Request reply for {upload_id}")
         await conn.emit('request_prediction_reply', {'uploadId': upload_id})
     except Exception as e:
-        logger.debug(f"Failed to request reply for {upload_id}: {e}")
+        logger.warning(f"Failed to request reply for {upload_id}: {e}")
+        logger.warning(e, exc_info=True)
         return False, e
 
     try:
@@ -111,7 +117,8 @@ async def wait_for_reply(conn, upload_id, fake):
         logger.debug(f"Received reply for {upload_id}: {reply}")
         return True, reply
     except Exception as e:
-        logger.debug(f"Failed to wait for reply for {upload_id}: {e}")
+        logger.warning(f"Failed to wait for reply for {upload_id}: {e}")
+        logger.warning(e, exc_info=True)
         return False, str(e)
 
 
@@ -135,8 +142,10 @@ async def send_feedback(upload_id, feedback, feedback_service_url, fake):
                     raise Exception(f"Failed to send feedback for {upload_id} to {feedback_service_url}")
         return True, None
     except Exception as e:
-        logger.debug(f"Failed to send feedback for {upload_id} to {feedback_service_url}: {e}")
+        logger.warning(f"Failed to send feedback for {upload_id} to {feedback_service_url}: {e}")
+        logger.warning(e, exc_info=True)
         return False, str(e)
+
 
 async def disconnect_ws(conn, fake):
     logger.debug(f"Disconnect from reply service")
@@ -149,5 +158,6 @@ async def disconnect_ws(conn, fake):
         await conn.disconnect()
         return True, None
     except Exception as e:
-        logger.debug(f"Failed to disconnect from reply service: {e}")
+        logger.warning(f"Failed to disconnect from reply service: {e}")
+        logger.warning(e, exc_info=True)
         return False, str(e)
