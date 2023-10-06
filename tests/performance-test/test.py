6023d8dc3c5ec6797ece7f9c1ff357f0b0596caf
Ali Ok <aliok@redhat.com>
2023-10-06 13:31:02 +0300

    Shite

--- tests/performance-test/test.py
+++ tests/performance-test/test.py
@@ -1,70 +1,37 @@
 import asyncio
 import json
-import time
-
 import logging
-import traceback
+import time
 
 from lib import upload, send_feedback, wait_for_reply, init_reply_ws_connection, disconnect_ws
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
-class SpanStore:
-    def __init__(self):
-        self.spans_by_name = {}  # name -> span count
-
-    def started(self, name):
-        if name not in self.spans_by_name:
-            self.spans_by_name[name] = 0
-        self.spans_by_name[name] += 1
-
-    def ended(self, name):
-        self.spans_by_name[name] -= 1
-        if self.spans_by_name[name] == 0:
-            del self.spans_by_name[name]
-
-    def summary(self):
-        out = ""
-        for name, count in self.spans_by_name.items():
-            out += f"{name}: {count}\n"
-        return out
 
 
 class Span:
-    def __init__(self, name, store, parent=None):
+    def __init__(self, name, parent=None):
         self.name = name
         self.start_time = None
         self.end_time = None
         self.children = []
-        self.store = store
         self.parent = parent
 
     def __enter__(self):
         self.start_time = time.time()
-        self.store.started(self.name)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end_time = time.time()
-        self.store.ended(self.name)
         if exc_val is not None:
             logger.warning(f"Exception in span {self.name}: {exc_val}")
             logger.exception(exc_tb)
             # logger.warning(exc_tb.format_exc())
         return True
 
-    def start(self, name):
-        s = Span(name, self.store, self)
-        s.start_time = time.time()
+    def subspan(self, name):
+        s = Span(name, self)
         self.children.append(s)
         return s
-
-    def end(self):
-        self.end_time = time.time()
-        self.store.ended(self.name)
-        return self.parent
 
     def print(self, indent=0):
         print(f"{' ' * indent}{self.name}: {self.end_time - self.start_time}")
@@ -72,10 +39,16 @@ class Span:
             child.print(indent + 2)
 
     def build_summary(self):
-        # name -> [duration]
-        summary = {
-            self.name: [self.end_time - self.start_time]
-        }
+        if self.end_time is None:
+            # name -> [duration]
+            summary = {
+                self.name: []
+            }
+        else:
+            # name -> [duration]
+            summary = {
+                self.name: [self.end_time - self.start_time]
+            }
 
         if len(self.children) != 0:
             for child in self.children:
@@ -102,27 +75,27 @@ class Client:
         image_path = image_data['image']
         feedback = image_data['feedback']
 
-        with root_span.start("client") as client_span:
+        with root_span.subspan("client") as client_span:
 
-            with client_span.start("init_ws_connection") as init_ws_connection_span:
+            with client_span.subspan("init_ws_connection") as init_ws_connection_span:
                 async with ws_req_semaphore:
-                    with init_ws_connection_span.start("do_init_ws_connection"):
+                    with init_ws_connection_span.subspan("do_init_ws_connection"):
                         ok, ws_conn = await init_reply_ws_connection(self.reply_service_url, self.fake)
                         if not ok:
                             logger.warning(f"Failed to init ws connection, stopping processing with client")
                             return
 
-            with client_span.start("upload") as upload_span:
+            with client_span.subspan("upload") as upload_span:
                 async with http_req_semaphore:
-                    with upload_span.start("send_upload_request"):
+                    with upload_span.subspan("send_upload_request"):
                         ok, upload_id = await upload(image_path, self.upload_service_url, self.fake)
                         if not ok:
                             logger.warning(f"Failed to upload {image_path}, stopping processing with client")
                             return
 
-            with client_span.start("reply") as reply_span:
+            with client_span.subspan("reply") as reply_span:
                 async with ws_req_semaphore:
-                    with reply_span.start("wait_for_reply"):
+                    with reply_span.subspan("wait_for_reply"):
                         ok, _ = await wait_for_reply(ws_conn, upload_id, self.fake)
                         if not ok:
                             logger.warning(f"Failed to wait for reply for {upload_id}, stopping processing with client")
@@ -133,13 +106,32 @@ class Client:
                                 f"Failed to disconnect from reply servicefor {upload_id} , stopping processing with client")
                             return
 
-            with client_span.start("feedback") as feedback_span:
+            with client_span.subspan("feedback") as feedback_span:
                 async with http_req_semaphore:
-                    with feedback_span.start("send_feedback_request") as send_feedback_request_span:
+                    with feedback_span.subspan("send_feedback_request"):
                         ok, _ = await send_feedback(upload_id, feedback, self.feedback_service_url, self.fake)
                         if not ok:
                             logger.warning(f"Failed to send feedback for {upload_id}, stopping processing with client")
                             return
+
+
+async def report(tg, root_span):
+    await asyncio.sleep(5)
+    in_progress_client_count = len(tg._tasks) - 1  # exclude this report task
+    loop = asyncio.get_running_loop()
+    scheduled_async_calls = len(loop._scheduled)
+    if in_progress_client_count > 0:
+        logger.info(
+            f"In progress client count: {in_progress_client_count}, scheduled_async_calls: {scheduled_async_calls}")
+
+        logger.info("Span summary for finished spans:")
+        span_summary = root_span.build_summary()
+        for span_name, durations in span_summary.items():
+            avg = sum(durations) / len(durations) if len(durations) > 0 else 0
+            logger.info(
+                f"\t{span_name:<80} ({len(durations):>5} executions) with average {avg:.4f} seconds")
+
+        tg.create_task(report(tg, root_span))
 
 
 async def runPass(total_client_count, concurrent_client_count, max_concurrent_ws_requests, max_concurrent_http_requests,
@@ -159,26 +151,17 @@ async def runPass(total_client_count, concurrent_client_count, max_concurrent_ws
 
     client_sem = asyncio.Semaphore(concurrent_client_count)
 
-    span_store = SpanStore()
-
-    with Span("root", span_store) as root_span:
+    with Span("root") as root_span:
         async def start_client(cl):
             async with client_sem:
                 await cl.start(root_span, http_req_semaphore, ws_req_semaphore)
 
         async with asyncio.TaskGroup() as tg:
+            tg.create_task(report(tg, root_span))
+
             for client in clients:
                 tg.create_task(start_client(client))
 
-    # print root span time
-    logger.info(f"Total time: {root_span.end_time - root_span.start_time}")
-
-    span_summary = root_span.build_summary()
-    for span_name, durations in span_summary.items():
-        logger.info(
-            f"{span_name:<80} ({len(durations):>5} executions) with average {sum(durations) / len(durations):.4f} seconds")
-
-    print(span_store.summary())
 
 async def main():
     # parser = argparse.ArgumentParser()
@@ -197,17 +180,18 @@ async def main():
     # args = parser.parse_args()
 
     args = {
-        'total_client_count': 5,
-        'concurrent_client_count': 50,
+        'total_client_count': 500,
+        'concurrent_client_count': 100,
         'max_concurrent_http_requests': 100,
         'max_concurrent_ws_requests': 100,
-        'upload_service': 'http://upload-service-ai-demo.apps.aliok-c145.serverless.devcluster.openshift.com',
-        'feedback_service': 'http://feedback-service-ai-demo.apps.aliok-c145.serverless.devcluster.openshift.com',
-        'reply_service': 'http://reply-service-ai-demo.apps.aliok-c145.serverless.devcluster.openshift.com',
+        'upload_service': 'http://upload-service-ai-demo.apps.aliok-c147.serverless.devcluster.openshift.com',
+        'feedback_service': 'http://feedback-service-ai-demo.apps.aliok-c147.serverless.devcluster.openshift.com',
+        'reply_service': 'http://reply-service-ai-demo.apps.aliok-c147.serverless.devcluster.openshift.com',
         'input': 'data.json',
-        'verbose': True,
-        # 'verbose': False,
-        'fake': True,
+        # 'verbose': True,
+        'verbose': False,
+        # 'fake': True,
+        'fake': False,
     }
 
     class FakeArgs:
@@ -220,6 +204,8 @@ async def main():
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
     await runPass(args.total_client_count, args.concurrent_client_count,
                   args.max_concurrent_ws_requests, args.max_concurrent_http_requests,
